81b3ce59032bdb5151775b78cc4f47c06cb679a8
q0abd <qiyomovabdulloh3@gmail.com>
2023-05-21 12:34:10 +0500

    movies added

--- src/components/MainSection/index.jsx
+++ src/components/MainSection/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Card } from "antd";
+const { Meta } = Card;
 
-const MainSection = () => {
-  return <div>MainSection</div>;
+const MainSection = ({ kinolar }) => {
+  return (
+    <div className="home-wrapper">
+      {kinolar.map((kino) => (
+        <Card
+          style={{
+            width: 300,
+          }}
+          cover={<img alt={kino.title} src={kino.image} />}
+          actions={[
+            <span>{kino.views}</span>,
+            <span>{kino.price}</span>,
+            <span>{kino.duration}</span>,
+          ]}
+        >
+          <Meta title={kino.title} description={kino.desc} />
+        </Card>
+      ))}
+    </div>
+  );
 };
 
 export default MainSection;
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -10,9 +10,77 @@ import Settings from "../components/Settings";
 import Dashboard from "../components/Dashboard";
 import Login from "../components/Login";
 
-const MainLayout = ({ kinolar }) => {
-  console.log(kinolar);
-
+const MainLayout = () => {
+  const movies = [
+    {
+      title: "Forsaj 10",
+      desc: "Eng yangi filmlardan",
+      image: "https://s5.vcdn.biz/static/f/5970245441/image.jpg",
+      views: "1.2m",
+      duration: "2:32m",
+      price: "Free",
+    },
+    {
+      title: "Spider man",
+      desc: "Yoshlikdagi eng super kinolarimdan",
+      image: "https://cdn.wallpapersafari.com/99/66/yokJde.jpg",
+      views: "1.6m",
+      duration: "2:15m",
+      price: "$12",
+    },
+    {
+      title: "Masha i Medved",
+      desc: "Multfilmni iti",
+      image:
+        "https://e1.pxfuel.com/desktop-wallpaper/167/197/desktop-wallpaper-s-masha-and-the-bear-masha-and-the-bear.jpg",
+      views: "521k",
+      duration: "1:54m",
+      price: "Free",
+    },
+    {
+      title: "Transformers",
+      desc: "Chotki lekin boru, maslahat bermiman korishni",
+      image:
+        "https://www.pixelstalk.net/wp-content/uploads/2016/05/HD-Transformers-Wallpapers-Pictures.jpg",
+      views: "2.1m",
+      duration: "2:01m",
+      price: "Free",
+    },
+    {
+      title: "Forsaj 10",
+      desc: "Eng yangi filmlardan",
+      image: "https://s5.vcdn.biz/static/f/5970245441/image.jpg",
+      views: "1.2m",
+      duration: "2:32m",
+      price: "Free",
+    },
+    {
+      title: "Spider man",
+      desc: "Yoshlikdagi eng super kinolarimdan",
+      image: "https://cdn.wallpapersafari.com/99/66/yokJde.jpg",
+      views: "1.6m",
+      duration: "2:15m",
+      price: "$12",
+    },
+    {
+      title: "Masha i Medved",
+      desc: "Multfilmni iti",
+      image:
+        "https://e1.pxfuel.com/desktop-wallpaper/167/197/desktop-wallpaper-s-masha-and-the-bear-masha-and-the-bear.jpg",
+      views: "521k",
+      duration: "1:54m",
+      price: "Free",
+    },
+    {
+      title: "Transformers",
+      desc: "Chotki lekin boru, maslahat bermiman korishni",
+      image:
+        "https://www.pixelstalk.net/wp-content/uploads/2016/05/HD-Transformers-Wallpapers-Pictures.jpg",
+      views: "2.1m",
+      duration: "2:01m",
+      price: "Free",
+    },
+  ];
   return (
     <>
       <header>
@@ -27,7 +95,7 @@ const MainLayout = ({ kinolar }) => {
             <Sidebar />
             <div className="main-wrapper text-light">
               <Routes>
-                <Route path="/" element={<MainSection />} />
+                <Route path="/" element={<MainSection kinolar={movies} />} />
                 <Route path="/new-posts" element={<NewPosts />} />
                 <Route path="/new-movies" element={<NewMovies />} />
                 <Route path="/news" element={<News />} />
